9ef87cde0885feaec8c02983cd544e31d1236971
jiangchen <1760247442@qq.com>
2023-10-16 00:13:13 +0800

    init

---
+++ src/main/java/tech/jiangchen/biz_base/BizBaseApplication.java
@@ -0,0 +1,13 @@
+package tech.jiangchen.biz_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BizBaseApplication.class, args);
+    }
+
+}
